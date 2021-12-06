31ffb191a4560b7bec4cb489cbbf072e7b777137
emt <emt@qq.com>
2021-12-06 22:31:31 +0800

    every day

--- js/birthday.js
+++ js/birthday.js
@@ -1,11 +1,13 @@
 
-  function link() {
-    var name = document.getElementById("name").value;
-    if(name == "lujia" || name == "卢佳"){
-      window.location.href = "card.html";
-    }else if(name === "zhaoguangyi" || name == "赵光仪"){
-      alert("写我名字干嘛,这是为你做的");
-    }else {
-      alert("不,你不是");
-    }
+function link() {
+  var name = document.getElementById("name").value;
+  if (name == "lujia" || name == "卢佳") {
+    window.location.href = "card.html";
+  } else if (name === "zhaoguangyi" || name == "赵光仪") {
+    alert("好感度+1？");
+  } else if (name.indexOf("赵光仪") != -1 && name.indexOf("卢佳") != 1) {
+    alert("我喜欢你！");
+  } else {
+    alert("不,你不是");
   }
+}
