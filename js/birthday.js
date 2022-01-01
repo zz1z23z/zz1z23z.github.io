be66b6916dde34477ccac1c93a77e30400da6bef
emt <emt@qq.com>
2022-01-01 10:30:35 +0800

    every day

--- js/birthday.js
+++ js/birthday.js
@@ -2,7 +2,7 @@
 function link() {
   var name = document.getElementById("name").value;
   if (name == "lujia" || name == "卢佳") {
-    window.location.href = "card.html";
+    window.location.href = "song.html";
   } else if (name === "zhaoguangyi" || name == "赵光仪") {
     alert("好感度+1？");
   } else if (name.indexOf("赵光仪") != -1 && name.indexOf("卢佳") != 1) {
